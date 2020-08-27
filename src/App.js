2b31a3306594590b4b03beb0df9d2bd4bc5f8f11
jito <j_ito@imin.co.jp>
2020-08-27 14:58:59 +0900

    learn satae and setState

--- src/App.js
+++ src/App.js
@@ -1,60 +1,32 @@
 import React, { Component } from 'react';
-import PropTypes from "prop-types"
 
-const App = () => {
-  const profiles = [
-    { name: "taro", age: 10 },
-    { name: "hanako", age: 5 },
-    { name: "jun", age: 20 },
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  )
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  handlePlusButton = () => {
+    return this.setState({ count: this.state.count + 1 })
+  }
+
+  handleMinusButton = () => {
+    return this.setState({ count: this.state.count - 1 })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count:{this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
+
 }
 
-const User = (props) => {
-  return (
-    <React.Fragment>
-      <p>Hi, I am {props.name} and {props.age} years old</p>
-    </React.Fragment>
-  )
-}
-
-// propsの型チェック
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number
-}
-
-// propsのデフォルト値
-// User.defaultProps = {
-//   age: 1
-// }
 
 export default App;
-
-// class App extends Component {
-//   render() {
-//     return React.createElement(
-//       "div",
-//       null,
-//       "Hello World"
-//     );
-//   }
-// }
-
-// function App() {
-//   return (
-//     <div>
-//       Hello World
-//     </div>
-//   );
-// }
-
-
